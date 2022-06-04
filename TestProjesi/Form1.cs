ac83a1cce432b1a5e429bfaafb26a983eade4d6f
bayramakgul <bayramakgul@gmail.com>
2022-06-04 23:04:51 +0300

    Güncellendi

--- TestProjesi/Form1.cs
+++ TestProjesi/Form1.cs
@@ -16,5 +16,10 @@ namespace TestProjesi
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Değişiklik yapıldı");
+        }
     }
 }
